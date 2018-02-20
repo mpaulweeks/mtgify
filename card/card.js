a80eeb3173e0fa3a4d323bf3218c97de3b6b1c53
mpaulweeks <mpaulweeks@gmail.com>
2018-02-20 16:40:56 -0500

    add card api for fetching multiverseid

--- card/card.js
+++ card/card.js
@@ -1,19 +1,55 @@
-class AutoCard extends HTMLElement {
+
+const CardAPI = new (class {
+  constructor() {
+    this.multiverse = new Promise((resolve, reject) => {
+      // todo real promise from fetch
+      resolve({'fog': '12345'})
+    })
+  }
+  normalizeCardName(cardName) {
+    return cardName.trim().toLowerCase()
+  }
+  performLookup(lookup, cardName) {
+    return lookup[this.normalizeCardName(cardName)]
+  }
+  getMultiverseId(cardName) {
+    const self = this
+    return self.multiverse
+      .then(lookup => self.performLookup(lookup, cardName))
+  }
+})()
+
+class _AutoCard extends HTMLElement {
   connectedCallback() {
-    this.addEventListener('click', e => {
-      this.openLink()
+    const self = this
+    self.addEventListener('click', e => {
+      self.openLink()
     });
 
-    this.name = this.innerHTML
+    // todo need true solution
+    setTimeout(() => {
+      self.name = self.innerHTML
+      CardAPI.getMultiverseId(self.name)
+        .then(mid => self.loadMultiverseId(mid))
+    }, 100);
   }
 
   getName() {
     return this.name
+  }
+  loadMultiverseId(mid) {
+    this.mid = mid
+    if (this.mid){
+      this.innerHTML += '#' + mid
+    }
   }
 
   openLink() {
     console.log(this)
   }
 }
+class CardText extends _AutoCard {}
+class CardMedium extends _AutoCard {}
 
-customElements.define('auto-card', AutoCard);
+customElements.define('card-text', CardText);
+customElements.define('card-medium', CardMedium);
