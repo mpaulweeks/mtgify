bf2f0c7eda378318d2e779983d069159e972411a
mpaulweeks <mpaulweeks@gmail.com>
2018-02-24 15:27:36 -1000

    add git pull to cronjob

--- bash/cronjob.sh
+++ bash/cronjob.sh
@@ -3,4 +3,5 @@
 # 0 * * * * ec2-user cd /home/ec2-user/mtg-toolbox && ./bash/cronjob.sh
 # pings version.json runs once per hour, downloads zips if there's an update
 
+git pull
 /home/ec2-user/.nvm/versions/node/v8.9.1/bin/node script/scrape.js
