bffb47e17824807e5f537afa0bdc5991b86cac7b
mpaulweeks <mpaulweeks@gmail.com>
2018-02-24 15:12:35 -1000

    update crontab doc

--- bash/cronjob.sh
+++ bash/cronjob.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-# 0 8 * * * ec2-user cd /home/ec2-user/mtg-toolbox && ./bash/cronjob.sh
+# 0 * * * * ec2-user cd /home/ec2-user/mtg-toolbox && ./bash/cronjob.sh
+# pings version.json runs once per hour, downloads zips if there's an update
 
 /home/ec2-user/.nvm/versions/node/v8.9.1/bin/node script/scrape.js
