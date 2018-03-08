480dc1e4a42b8060bf626b440eb1653008d2efc2
mpaulweeks <mpaulweeks@gmail.com>
2018-03-08 16:58:27 -0500

    add homepage with info and snippet creator

---
+++ docs/home.js
@@ -0,0 +1,23 @@
+
+const config = {
+  enableAutoTag: true,
+};
+
+const imgSource = document.getElementById('imgSource');
+const linkSource = document.getElementById('linkSource');
+const snippetExmaple = document.getElementById('snippetExample');
+const snippetLink = document.getElementById('snippetLink');
+
+function renderSnippet(){
+  config.imgSource = imgSource.value;
+  config.linkSource = linkSource.value;
+
+  snippetLink.setAttribute('href', `javascript:(function(){window.AUTOCARD_CONFIG=${JSON.stringify(config)};document.body.appendChild(document.createElement('script')).src='https://autocard.mpaulweeks.com/dist/autocard.js';})();`);
+
+  snippetExample.innerHTML = 'Force of Will';
+  window.AUTOCARD.tagElement(snippetExample, config);
+}
+
+imgSource.onchange = renderSnippet;
+linkSource.onchange = renderSnippet;
+renderSnippet();
