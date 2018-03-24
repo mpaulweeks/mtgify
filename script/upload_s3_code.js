02232aa7e8ca7f5da5a44f61b7314324908b4a98
mpaulweeks <mpaulweeks@gmail.com>
2018-03-24 12:48:47 -0400

    move index.html to root dir

--- script/upload_s3_code.js
+++ script/upload_s3_code.js
@@ -4,5 +4,7 @@ const CDN = require('../lib/CDN')
 
 CDN.uploadFolder('dist')
   .then(msg => console.log(msg))
+  .then(() => CDN.uploadFile('index.html'))
+  .then(msg => console.log(msg))
   .then(() => CDN.uploadFolder('docs'))
   .then(msg => console.log(msg))
