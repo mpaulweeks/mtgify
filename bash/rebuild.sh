f3da58ea7a3d4fa4e1299589c442afbc4b73621b
mpaulweeks <mpaulweeks@gmail.com>
2020-07-18 18:53:19 -0400

    update rebuild

--- bash/rebuild.sh
+++ bash/rebuild.sh
@@ -5,9 +5,7 @@ export $(cat .env* | grep -v ^# | xargs)
 
 # https://github.com/npm/npm/issues/17722
 npm install
-npm prune
 git checkout -- package-lock.json
 
-npm run build
 npm run cron-code
 npm run cron-json
