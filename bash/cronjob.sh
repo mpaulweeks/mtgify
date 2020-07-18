7745a22e50f6664c57ad572644ee1524837691e2
mpaulweeks <mpaulweeks@gmail.com>
2020-07-18 17:01:42 -0400

    simplify cron scripts

--- bash/cronjob.sh
+++ bash/cronjob.sh
@@ -1,7 +1,7 @@
 # crontab
-# */5 * * * * ec2-user cd /home/ec2-user/mtg-toolbox && ./bash/cronjob.sh
-# pulls source, rebuilds js and json if change
-# pings version.json, downloads and rebuilds json if update
+# 0 8 * * * ec2-user cd /home/ec2-user/mtgify && ./bash/cronjob.sh
+# pulls source, executes rebuild.sh
 
-./bash/cron_pull.sh
-./bash/cron_scrape.sh
+git checkout master
+git pull
+./bash/rebuild.sh
--- bash/rebuild.sh
+++ bash/rebuild.sh
@@ -1,3 +1,5 @@
+# rebuild code, gen everything, upload everything
+
 export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v8.9.1/bin/
 export $(cat .env* | grep -v ^# | xargs)
 
@@ -7,6 +9,5 @@ npm prune
 git checkout -- package-lock.json
 
 npm run build
-node script/upload_s3_code.js
-
-node script/gen_json.js && node script/upload_s3_json.js
+npm run cron-code
+npm run cron-json
