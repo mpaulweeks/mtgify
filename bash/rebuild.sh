49189db4b5b0cdd980710dfaf1fc0f38be2230ad
mpaulweeks <mpaulweeks@gmail.com>
2020-08-31 14:09:22 -0400

    debugging

--- bash/rebuild.sh
+++ bash/rebuild.sh
@@ -1,9 +1,10 @@
 # rebuild code, gen everything, upload everything
 
 nvm use 10.16.0
+export GOOGLE_APPLICATION_CREDENTIALS="gcp-key.json"
 
 # export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v10.16.0/bin/
-export $(cat .env* | grep -v ^# | xargs)
+# export $(cat .env* | grep -v ^# | xargs)
 
 # https://github.com/npm/npm/issues/17722
 npm install
