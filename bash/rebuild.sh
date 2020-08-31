ef38fa1c2f589330ff6671e363808a7b1dbc7981
mpaulweeks <mpaulweeks@gmail.com>
2020-08-31 14:15:42 -0400

    fix nvm access from cron

--- bash/rebuild.sh
+++ bash/rebuild.sh
@@ -1,10 +1,13 @@
 # rebuild code, gen everything, upload everything
 
+# https://devimalplanet.com/using-nvm-in-cron-jobs
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# setup env
 nvm use 10.16.0
 export GOOGLE_APPLICATION_CREDENTIALS="gcp-key.json"
-
-# export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v10.16.0/bin/
-# export $(cat .env* | grep -v ^# | xargs)
 
 # https://github.com/npm/npm/issues/17722
 npm install
